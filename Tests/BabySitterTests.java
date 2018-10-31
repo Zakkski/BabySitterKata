03617bc113527b3afd7a0f9b3a4da66733932349
unknown <Zachary.Kwiecinski@gmail.com>
2018-10-30 22:54:03 -0400

    First test with family A

--- Tests/BabySitterTests.java
+++ Tests/BabySitterTests.java
@@ -1,10 +1,19 @@
 import com.preludes.babysitter.BabySitter;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BabySitterTests {
 
+    //Testing to make sure class creation doesn't cause any errors
     @Test
     public void mainClassCreation(){
         BabySitter baby = new BabySitter();
+    }
+
+    //Tests from 5pm - 6pm with Family A
+    @Test
+    public void workingOneHourWithFamilyA(){
+        BabySitter baby = new BabySitter();
+        Assert.assertEquals(15, baby.sittingJob(5, 6));
     }
 }
--- src/com/preludes/babysitter/BabySitter.java
+++ src/com/preludes/babysitter/BabySitter.java
@@ -1,4 +1,9 @@
 package com.preludes.babysitter;
 
 public class BabySitter {
+
+    //Method to input times of work
+    public int sittingJob(int startTime, int endTime){
+        return 15;
+    }
 }
